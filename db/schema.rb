a046cda770f42e05a49fa400415b5d07dd0c0f6c
William Swift <bswift89@gmail.com>
2014-01-22 18:54:39 -0500

    Add image upload with Paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121152303) do
+ActiveRecord::Schema.define(version: 20140122204917) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,7 +35,15 @@ ActiveRecord::Schema.define(version: 20140121152303) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_id"
+    t.string   "integer"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
+
+  add_index "pins", ["integer"], name: "index_pins_on_integer"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
